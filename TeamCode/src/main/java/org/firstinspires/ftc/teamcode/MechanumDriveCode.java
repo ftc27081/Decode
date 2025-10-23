4e38cbe737cb44938f3660aa8a10242b3ced3cff
saranthummati <saran.thummati@gmail.com>
2025-10-22 23:11:44 -0700

    Latest working code 10.22

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDriveCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDriveCode.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
