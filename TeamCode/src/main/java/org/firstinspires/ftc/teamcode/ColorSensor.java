66b26e559338b9cfddb1fb2fd8160e8d775a1413
sunay <bananaman6127@gmail.com>
2025-12-07 18:42:35 -0800

    C67

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensor.java
@@ -14,28 +14,33 @@ public class ColorSensor {
         PURPLE,
         UNKNOWN
     }
-    public void init (HardwareMap hwMap) {
-        colorSensor = hwMap.get(NormalizedColorSensor.class, ""); //Put name of sensor in control hub in quotations
-
+    public void init (HardwareMap hwMap){
+        colorSensor = hwMap.get(NormalizedColorSensor.class, "ColorSensor"); //Put name of sensor in control hub in quotations
+        colorSensor.setGain(5);
     }
     public DetectedColor getDetectedColor(Telemetry telemetry) {
         NormalizedRGBA colors = colorSensor.getNormalizedColors(); //will return the 4 earlier values
 
-        float normGREEN, normPURPLE;
-        normGREEN = colors.green / colors.alpha;
-        normPURPLE = colors.red + colors.blue / colors.alpha;
+        float normGreen, normPurple;
+        normGreen = colors.green / colors.alpha;
+        normPurple = colors.red + colors.blue / colors.alpha;
 
-        telemetry.addData("Green", normGREEN);
-        telemetry.addData("Purple", normPURPLE);
+        telemetry.addData("green", normGreen);
+        telemetry.addData("purple", normPurple);
 
         //add if statements for specific colors added
         /*
         Green, Purple
-        GREEN =
-        PURPLE =
-         */
+        GREEN = > .4, <.3,                                      // ADD the restriction shownd with the adjusted set gain.
+        PURPLE = > .4, <.3,
 
-        return DetectedColor.UNKNOWN;
+        if(normPurple > 0.4 && normGreen <0.3){
+            return DetectedColor.PURPLE;                       // change numers here aswell
+        } else if (normPurple < 0.3 && normGreen > 0.4){
+        return DetectedColor.GREEN;                             //Once imported all the numbers and adjusted the if commanments. Uncomment these lines and comment out lines 28-29
+        }
+        else if */{         return DetectedColor.UNKNOWN;
+        }
 
     }
 }
